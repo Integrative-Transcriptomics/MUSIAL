1b75d9901e369e8466d85086eebff5fe8bf0816b
s-t-h <simon.hackl@student.uni-tuebingen.de>
2021-09-02 10:45:08 +0200

    [ADD] added JavaDoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"datastructure"},{"l":"exceptions"},{"l":"main"},{"l":"runnables"},{"l":"tools"},{"l":"utility"}];updateSearchResults();
