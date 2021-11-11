f4fb5d1c1ab2829a0a572f40027e8665e06fd370
s-t-h <simon.hackl@student.uni-tuebingen.de>
2021-11-11 10:25:48 +0100

    [UPDATE] Adapted SampleAnalysisEntry.java class to match changes in other classes.

--- src/main/java/datastructure/SampleAnalysisEntry.java
+++ src/main/java/datastructure/SampleAnalysisEntry.java
@@ -5,7 +5,7 @@ import htsjdk.variant.vcf.VCFFileReader;
 /**
  * Internal representation of a sample that is subject to analysis.
  * <p>
- * Pairs of instances of this class and {@link ReferenceAnalysisEntry} instances are used internally as so called 'Run
+ * Pairs of instances of this class and {@link FeatureAnalysisEntry} instances are used internally as so called 'Run
  * entries' to specify a set of analysis tasks, i.e. which sample is analyzed with respect to which specified
  * reference feature.
  *
