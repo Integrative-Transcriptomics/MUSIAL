23a190a070c111d5e7610a65472a77e5878d5380
s-t-h <simon.hackl@student.uni-tuebingen.de>
2022-05-27 15:44:22 +0200

    [UPDATE] Start of new developement branch; * Deleted ./doc and ./example directories for easier version control.

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"components"},{"l":"datastructure"},{"l":"exceptions"},{"l":"main"},{"l":"runnables"},{"l":"utility"}];updateSearchResults();
