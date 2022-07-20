a9068a67593c15b31664156128e7a2ca27b6f04b
s-t-h <si.ha@live.de>
2022-07-20 11:37:28 +0200

    [UPDATE] Updated JavaDoc to current version 2.1-dev.

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"cli"},{"l":"components"},{"l":"datastructure"},{"l":"exceptions"},{"l":"main"},{"l":"runnables"}];updateSearchResults();
