87e8ae2cb24e67ed2c2edd5bff16490cdf07c477
s-t-h <simon.hackl@student.uni-tuebingen.de>
2022-10-26 14:15:00 +0200

    [UPDATE] Split VariantsDictionaryFactory build method into build and load methods.

--- src/main/java/components/VariantsDictionaryFactory.java
+++ src/main/java/components/VariantsDictionaryFactory.java
@@ -6,6 +6,7 @@ import datastructure.VariantsDictionary;
 import exceptions.MusialException;
 import runnables.SampleAnalyzerRunnable;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -23,7 +24,7 @@ public final class VariantsDictionaryFactory {
     /**
      * Constructs and returns a new {@link VariantsDictionary} instance from the information specified by the {@link ModuleBuildParameters} instance.
      *
-     * @param configuration {@link ModuleBuildParameters} instance yielding parameter specification for the MUSIAL update variants dictionary module.
+     * @param configuration {@link ModuleBuildParameters} instance yielding parameter specification for the MUSIAL BUILD module.
      * @return {@link VariantsDictionary} instance.
      * @throws IOException     Thrown if any input or output file is missing or unable to being generated (caused by any native Java method).
      * @throws MusialException Thrown if any method fails wrt. internal logic, i.e. assignment of proteins to genomes.
@@ -31,13 +32,28 @@ public final class VariantsDictionaryFactory {
     public static VariantsDictionary build(ModuleBuildParameters configuration)
             throws IOException, MusialException {
         if (configuration.outputFile.exists()) {
-            Logging.logStatus("Update existing variants dictionary at " + Logging.colorParameter(configuration.outputFile.getAbsolutePath()));
-            // TODO: In the current implementation, the parameters of the variant dictionary are not overwritten!
-            return IO.readVariantsDictionary(configuration.outputFile);
+            throw new MusialException("(I/O) Specified file (" + configuration.outputFile.getAbsolutePath() + ") to build variants dictionary already exists.");
         } else {
             Logging.logStatus("Generate new variants dictionary at " + Logging.colorParameter(configuration.outputFile.getAbsolutePath()));
             return new VariantsDictionary(configuration.minCoverage, configuration.minHomFrequency, configuration.minHetFrequency,
                     configuration.maxHetFrequency, configuration.minQuality);
+        }
+    }
+
+    /**
+     * Parses an existing {@link VariantsDictionary} file and returns it.
+     *
+     * @param inputFile {@link File} instance pointing to the file that contains the variants dictionary content.
+     * @return {@link VariantsDictionary} instance.
+     * @throws MusialException Thrown if any method fails wrt. internal logic, i.e. assignment of proteins to genomes.
+     */
+    public static VariantsDictionary load(File inputFile)
+            throws MusialException, IOException {
+        if (!inputFile.exists()) {
+            throw new MusialException("(I/O) Specified file (" + inputFile.getAbsolutePath() + ") to load variants dictionary does not exist.");
+        } else {
+            Logging.logStatus("Load variants dictionary from " + Logging.colorParameter(inputFile.getAbsolutePath()));
+            return IO.readVariantsDictionary(inputFile);
         }
     }
 
