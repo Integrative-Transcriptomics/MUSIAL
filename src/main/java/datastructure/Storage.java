a53be06ac8afd8db62c82082e8767052552c2c69
s-t-h <simon.hackl@uni-tuebingen.de>
2025-08-07 13:13:21 +0200

    Remove automated clustering implementation; this will be re-implemented as separate task

--- src/main/java/datastructure/Storage.java
+++ src/main/java/datastructure/Storage.java
@@ -1083,56 +1083,6 @@ public class Storage {
                 }
             }
         }
-
-        // Perform clustering for alleles and proteoforms per feature.
-        for (Feature feature : getFeatures()) {
-            // Reset clustering and add alleles to the dataset.
-            Clustering.reset();
-            feature.getAlleles().forEach(allele ->
-                    Clustering.addToDataset(allele.uid, allele.getVariants())
-            );
-
-            if (Clustering.hasData()) {
-                // Train the clustering model.
-                Clustering.train();
-
-                // Process clustering results for alleles.
-                Clustering.getClusteringResult().forEach(entry -> {
-                    // Retrieve the allele by its name and update its attributes with clustering results.
-                    Feature.Allele allele = feature.getAllele(entry.name());
-                    allele.setAttribute(Constants.$SequenceType_clusterLabel, String.valueOf(entry.label()));
-                    allele.setAttribute(Constants.$SequenceType_clusterIndex, String.valueOf(entry.idx()));
-                    allele.setAttribute(Constants.$SequenceType_clusterOutlierScore, String.format(Locale.US, "%.3f", entry.outlierScore()));
-                    // Update the allele's name to include clustering information.
-                    allele.setName("%s.a%s.%s".formatted(feature.name, entry.idx(), entry.label()));
-                });
-            }
-
-            // If the feature is coding, process proteoforms.
-            if (feature.isCoding() && !skipProteoformInference()) {
-                // Reset clustering and add proteoforms to the dataset.
-                Clustering.reset();
-                feature.getProteoforms().forEach(proteoform ->
-                        Clustering.addToDataset(proteoform.uid, proteoform.getVariants())
-                );
-
-                if (Clustering.hasData()) {
-                    // Train the clustering model.
-                    Clustering.train();
-
-                    // Process clustering results for proteoforms.
-                    Clustering.getClusteringResult().forEach(entry -> {
-                        // Retrieve the proteoform by its name and update its attributes with clustering results.
-                        Feature.Proteoform proteoform = feature.getProteoform(entry.name());
-                        proteoform.setAttribute(Constants.$SequenceType_clusterLabel, String.valueOf(entry.label()));
-                        proteoform.setAttribute(Constants.$SequenceType_clusterIndex, String.valueOf(entry.idx()));
-                        proteoform.setAttribute(Constants.$SequenceType_clusterOutlierScore, String.format(Locale.US, "%.3f", entry.outlierScore()));
-                        // Update the proteoform's name to include clustering information.
-                        proteoform.setName("%s.p%s.%s".formatted(feature.name, entry.idx(), entry.label()));
-                    });
-                }
-            }
-        }
     }
 
     /**
